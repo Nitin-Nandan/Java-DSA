f97316e87fdea5fcf3c5e17a67d6e61bf53fde91
Nitin Nandan <nitinkrs1980@gmail.com>
2025-05-08 20:24:38 +0530

    SC Menu

---
+++ Conditional_Statements/Menu.java
@@ -0,0 +1,42 @@
+package Conditional_Statements;
+
+// Create a Simple Menu Program without GUI to perform ADD, SUB, MUL, DIV
+
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String arg[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Menu :\nADD\nSUB\nMUL\nDIV");
+        System.out.print("Enter two numbers: ");
+        float num1 = sc.nextFloat();
+        float num2 = sc.nextFloat();
+        String ipBuffer = sc.nextLine();
+        System.out.print("Enter menu option in words: ");
+        String option = sc.nextLine();
+
+        switch (option) {
+            case "ADD":
+                System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+                break;
+            case "SUB":
+                System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+                break;
+            case "MUL":
+                System.out.println(num1 + " x " + num2 + " = " + (num1 * num2));
+                break;
+            case "DIV":
+                if (num2 == 0) {
+                    System.out.println("Divide by 0");
+                    break;
+                } else {
+                    System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+                    break;
+                }
+            default:
+                System.out.println("Please enter a valid Menu option.");
+                break;
+        }
+        sc.close();
+    }
+}
