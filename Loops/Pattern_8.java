5e4c34c7ce3a8f57df87d09af25f05f911a09817
Nitin Nandan <nitinkrs1980@gmail.com>
2025-05-10 17:46:55 +0530

    SC Pattern_8

---
+++ Loops/Pattern_8.java
@@ -0,0 +1,25 @@
+// Create the following pattern:
+//         *
+//       * * *
+//     * * * * *
+//   * * * * * * *     
+// * * * * * * * * *
+//   * * * * * * *
+//     * * * * *
+//       * * * 
+//         *
+
+public class Pattern_8 {
+    public static void main(String arg[]) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (j <= 5-i) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
